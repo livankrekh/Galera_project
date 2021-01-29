db5f1856faa71519465bae2e8fad89f3b3a6b6c1
Nagatsuka Akiya <nagatsuka.akiya@gmail.com>
2021-01-29 15:09:03 +0900

    Fix post

--- server/ml_model/model_base.py
+++ server/ml_model/model_base.py
@@ -17,7 +17,7 @@ def transform_image(image_bytes):
                                         transforms.Normalize(
                                             [0.485, 0.456, 0.406],
                                             [0.229, 0.224, 0.225])])
-    image = Image.open(io.BytesIO(image_bytes))
+    image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
     return my_transforms(image).unsqueeze(0)
 
 
